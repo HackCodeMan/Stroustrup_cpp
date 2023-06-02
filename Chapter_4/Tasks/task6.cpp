ea9f1bb40d03e48cd39e7c4590b8ab0c0e736d84
Anton Pokrovskiy <pokrantnicol@gmail.com>
2023-06-02 11:01:04 +0500

    Add task6-7 modificed task1-5

---
+++ Chapter_4/Tasks/task6.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <windows.h>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+    SetConsoleCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
+
+    string is_end = "";
+    while (is_end != "|")
+    {
+        vector<double> numbers;
+        for (int i {0}; i < 10; i++)
+        {
+            double num;
+            cin >> num;
+
+            
+        }
+    }
+    return 0;
+}
